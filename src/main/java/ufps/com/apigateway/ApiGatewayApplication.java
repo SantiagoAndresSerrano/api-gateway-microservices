15be613c8b860a91a018118810e63d4ccaf03273
Santiago Serrano <62350078+SantiagoAndresSerrano@users.noreply.github.com>
2022-11-13 14:01:15 -0600

    Update ApiGatewayApplication.java

--- src/main/java/ufps/com/apigateway/ApiGatewayApplication.java
+++ src/main/java/ufps/com/apigateway/ApiGatewayApplication.java
@@ -24,20 +24,6 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PUT");
-        config.setMaxAge(18000L);
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+    
 
 }
